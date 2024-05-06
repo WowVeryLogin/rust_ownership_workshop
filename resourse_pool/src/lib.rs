b4d22347ab52e31727e32574aa8eb6faf466840a
Denis Davydov <ddavydov@cloudflare.com>
2024-05-06 16:56:31 +0100

    Fix resource task

--- resourse_pool/src/lib.rs
+++ resourse_pool/src/lib.rs
@@ -10,19 +10,15 @@
 // Tip:
 // You can use dashmap::DashMap as hashmap to simplify working with maps;
 
-// Feel free to create/remove any necessary internal structures
-
-// Extra hometask: add postpone mode. What it means is that resource is not deleted immediatelly after everyone stopped using it
-// it stays in the cache for resource::RESOURCE_TTL_MS;
-// During this period consumers can get it again without construction (if this happens deletion is canceled).
-// Tip for hometask:
-// You can spawn async tasks with tokio::task::spawn_local;
-
 use dashmap::DashMap;
 use std::rc::Rc;
 mod resource;
 use resource::{ExpensiveResource, Uuid};
 
+// Feel free to create/remove any necessary internal structures
+pub struct ExpensiveResourceWrapped {}
+
+impl ExpensiveResourceWrapped {}
 
 struct Pool {
     // Your code here
@@ -30,13 +26,12 @@ struct Pool {
 
 impl Pool {
     fn new() -> Self {
-        Self {
-        }
+        Self {}
     }
 
-    fn get_resource(self: &Self, uuid: Uuid) -> Rc<ExpensiveResource> {
+    fn get_resource(self: Rc<Self>, uuid: Uuid) -> Rc<ExpensiveResourceWrapped> {
         // Your code here
-        Rc::new(ExpensiveResource::new(uuid))
+        Rc::new(ExpensiveResourceWrapped {})
     }
 }
 
@@ -47,92 +42,106 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let p = Pool::new();
+        let p = Rc::new(Pool::new());
         {
-            let r1 = p.get_resource(100);
-            let r2= p.get_resource(100);
+            let _r1 = p.clone().get_resource(100);
+            let _r2 = p.clone().get_resource(100);
         }
         {
-            let r1 = p.get_resource(100);
-            let r2 = p.get_resource(200);
-            let r3= p.get_resource(200);
+            let _r1 = p.clone().get_resource(100);
+            let _r2 = p.clone().get_resource(200);
+            let _r3 = p.clone().get_resource(200);
         }
         let v = GLOBAL_RESOURCE_MAP.get(&100).unwrap();
         assert_eq!(v.0, 2);
-        let v = GLOBAL_RESOURCE_MAP.get(&100).unwrap();
+        assert_eq!(v.1, 2);
+        let v = GLOBAL_RESOURCE_MAP.get(&200).unwrap();
         assert_eq!(v.0, 1);
+        assert_eq!(v.1, 1);
     }
-
-    // #[test]
-    // fn postponed_mode_test() {
-    //     let rt = tokio::runtime::Builder::new_current_thread()
-    //         .enable_time()
-    //         .build()
-    //         .unwrap();
-    //     rt.block_on(async {
-    //         let mp = Rc::new(Pool::new());
-    //         let set = tokio::task::LocalSet::new();
-
-    //         // create (10..20) resources
-    //         (10..20).for_each(|i| {
-    //             let mp = mp.clone();
-    //             set.spawn_local(async move {
-    //                 println!("asking for resource {}", i);
-    //                 let r = mp.get_resource(i);
-    //                 tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS * 2)).await;
-    //                 println!(
-    //                     "sleept for {} and dropping resource {}",
-    //                     RESOURCE_TTL_MS * 2,
-    //                     i
-    //                 );
-    //                 drop(r);
-    //             });
-    //         });
-
-    //         // create (0..10) resources and reuse (10..20) resources
-    //         (0..20).for_each(|i| {
-    //             let mp = mp.clone();
-    //             set.spawn_local({
-    //                 async move {
-    //                     tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS / 3))
-    //                         .await;
-    //                     println!(
-    //                         "sleept for {} and asking for resource {}",
-    //                         RESOURCE_TTL_MS / 3,
-    //                         i
-    //                     );
-    //                     mp.get_resource(i);
-    //                 }
-    //             });
-    //         });
-
-    //         // reuse (0..10) resources
-    //         (0..10).for_each(|i| {
-    //             let mp = mp.clone();
-    //             set.spawn_local(async move {
-    //                 tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS / 2)).await;
-    //                 println!(
-    //                     "sleept for {} and asking for resource {}",
-    //                     RESOURCE_TTL_MS / 2,
-    //                     i
-    //                 );
-    //                 mp.get_resource(i);
-    //             });
-    //         });
-
-    //         set.await;
-    //         // check that each resource created only once;
-    //         (0..20).for_each(|i| {
-    //             let v = GLOBAL_RESOURCE_MAP.get(&i).unwrap();
-    //             assert_eq!(v.0, 1);
-    //         });
-
-    //         tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS)).await;
-    //         // check that all resources are deleted once;
-    //         (0..20).for_each(|i| {
-    //             let v = GLOBAL_RESOURCE_MAP.get(&i).unwrap();
-    //             assert_eq!(v.1, 1);
-    //         });
-    //     });
-    // }
 }
+
+// Extra hometask: add postpone mode. What it means is that resource is not deleted immediatelly after everyone stopped using it
+// it stays in the cache for resource::RESOURCE_TTL_MS;
+// During this period consumers can get it again without construction (if this happens deletion is canceled).
+// Tip for hometask:
+// You can spawn async tasks with tokio::task::spawn_local;
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use resource::GLOBAL_RESOURCE_MAP;
+
+//     #[test]
+//     fn postponed_mode_test() {
+//         let rt = tokio::runtime::Builder::new_current_thread()
+//             .enable_time()
+//             .build()
+//             .unwrap();
+//         rt.block_on(async {
+//             let mp = Rc::new(Pool::new());
+//             let set = tokio::task::LocalSet::new();
+
+//             // create (10..20) resources
+//             (10..20).for_each(|i| {
+//                 let mp = mp.clone();
+//                 set.spawn_local(async move {
+//                     println!("asking for resource {}", i);
+//                     let r = mp.get_resource(i);
+//                     tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS * 2)).await;
+//                     println!(
+//                         "sleept for {} and dropping resource {}",
+//                         RESOURCE_TTL_MS * 2,
+//                         i
+//                     );
+//                     drop(r);
+//                 });
+//             });
+
+//             // create (0..10) resources and reuse (10..20) resources
+//             (0..20).for_each(|i| {
+//                 let mp = mp.clone();
+//                 set.spawn_local({
+//                     async move {
+//                         tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS / 3))
+//                             .await;
+//                         println!(
+//                             "sleept for {} and asking for resource {}",
+//                             RESOURCE_TTL_MS / 3,
+//                             i
+//                         );
+//                         mp.get_resource(i);
+//                     }
+//                 });
+//             });
+
+//             // reuse (0..10) resources
+//             (0..10).for_each(|i| {
+//                 let mp = mp.clone();
+//                 set.spawn_local(async move {
+//                     tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS / 2)).await;
+//                     println!(
+//                         "sleept for {} and asking for resource {}",
+//                         RESOURCE_TTL_MS / 2,
+//                         i
+//                     );
+//                     mp.get_resource(i);
+//                 });
+//             });
+
+//             set.await;
+//             // check that each resource created only once;
+//             (0..20).for_each(|i| {
+//                 let v = GLOBAL_RESOURCE_MAP.get(&i).unwrap();
+//                 assert_eq!(v.0, 1);
+//             });
+
+//             tokio::time::sleep(std::time::Duration::from_millis(RESOURCE_TTL_MS)).await;
+//             // check that all resources are deleted once;
+//             (0..20).for_each(|i| {
+//                 let v = GLOBAL_RESOURCE_MAP.get(&i).unwrap();
+//                 assert_eq!(v.1, 1);
+//             });
+//         });
+//     }
+// }
